2a7e47ede2e3121e60dfca2179e422b29990eefa
ophiraShen <ophira.shenyige@outlook.com>
2024-12-12 21:33:52 +0800

    lora glm_edge_1.5b

--- glm_edge_1.5b_chat/train.sh
+++ glm_edge_1.5b_chat/train.sh
@@ -3,7 +3,7 @@
 set -ex
 
 # 基础配置
-LR=2e-4
+LR=8e-5
 
 DATESTR=`date +%Y%m%d-%H%M%S`
 RUN_NAME=glm_edge_1.5b_chat
@@ -11,7 +11,7 @@ OUTPUT_DIR=output/${RUN_NAME}-${DATESTR}
 mkdir -p $OUTPUT_DIR
 
 # 模型路径
-MODEL_PATH="/root/autodl-tmp/THUDM/glm-edge-1.5b-chat"
+MODEL_PATH="/root/autodl-fs/modelscope"
 
 CUDA_VISIBLE_DEVICES=0 python finetune.py \
     --do_train \
@@ -22,20 +22,20 @@ CUDA_VISIBLE_DEVICES=0 python finetune.py \
     --response_column output \
     --model_name_or_path "${MODEL_PATH}" \
     --output_dir $OUTPUT_DIR \
-    --max_source_length 1024 \ 
+    --max_source_length 1024 \
     --max_target_length 256 \
     --per_device_train_batch_size 8 \
     --per_device_eval_batch_size 8 \
     --gradient_accumulation_steps 4 \
     --evaluation_strategy steps \
     --eval_steps 90 \
-    --num_train_epochs 3 \
+    --num_train_epochs 5 \
     --logging_steps 10 \
     --logging_dir $OUTPUT_DIR/logs \
     --save_steps 90 \
     --learning_rate $LR \
-    --warmup_ratio 0.1 \
-    --lora_rank 8 \
-    --lora_alpha 32 \
-    --lora_dropout 0.1 2>&1 | tee ${OUTPUT_DIR}/train.log
+    # --warmup_ratio 0.15 \
+    --lora_rank 16 \
+    --lora_alpha 64 \
+    --lora_dropout 0.1 \
     --fp16 2>&1 | tee ${OUTPUT_DIR}/fp16.log
