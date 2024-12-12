145bfec63a3da341f6a1f5469af792f33ce074d2
ophiraShen <ophira.shenyige@outlook.com>
2024-12-12 17:07:22 +0800

    trainning sets

---
+++ glm_edge_1.5b_chat/train.sh
@@ -0,0 +1,41 @@
+#! /usr/bin/env bash
+
+set -ex
+
+# 基础配置
+LR=2e-4
+
+DATESTR=`date +%Y%m%d-%H%M%S`
+RUN_NAME=glm_edge_1.5b_chat
+OUTPUT_DIR=output/${RUN_NAME}-${DATESTR}
+mkdir -p $OUTPUT_DIR
+
+# 模型路径
+MODEL_PATH="/root/autodl-tmp/THUDM/glm-edge-1.5b-chat"
+
+CUDA_VISIBLE_DEVICES=0 python finetune.py \
+    --do_train \
+    --do_eval \
+    --train_file ../data/train.jsonl \
+    --validation_file ../data/val.jsonl \
+    --prompt_column input \
+    --response_column output \
+    --model_name_or_path "${MODEL_PATH}" \
+    --output_dir $OUTPUT_DIR \
+    --max_source_length 1024 \ 
+    --max_target_length 256 \
+    --per_device_train_batch_size 8 \
+    --per_device_eval_batch_size 8 \
+    --gradient_accumulation_steps 4 \
+    --evaluation_strategy steps \
+    --eval_steps 90 \
+    --num_train_epochs 3 \
+    --logging_steps 10 \
+    --logging_dir $OUTPUT_DIR/logs \
+    --save_steps 90 \
+    --learning_rate $LR \
+    --warmup_ratio 0.1 \
+    --lora_rank 8 \
+    --lora_alpha 32 \
+    --lora_dropout 0.1 2>&1 | tee ${OUTPUT_DIR}/train.log
+    --fp16 2>&1 | tee ${OUTPUT_DIR}/fp16.log
